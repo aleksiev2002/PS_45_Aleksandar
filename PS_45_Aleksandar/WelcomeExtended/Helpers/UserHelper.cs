a532785bbf831ab1c396559248aa8a0e9b540f5a
Needlessbug <100784917+aleksiev2002@users.noreply.github.com>
2024-03-14 19:36:11 +0200

    added 3

--- PS_45_Aleksandar/WelcomeExtended/Helpers/UserHelper.cs
+++ PS_45_Aleksandar/WelcomeExtended/Helpers/UserHelper.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Welcome.Model;
+using WelcomeExtended.Data;
 
 namespace WelcomeExtended.Helpers
 {
-    internal class UserHelper
+    public static class UserHelper
     {
+        public static string ToStringExtension(this User user)
+        {
+            return $"Id: {user.Id}, Name: {user.Names}, Role: {user.Role}, Expires: {user.Expires?.ToString("yyyy-MM-dd") ?? "N/A"}";
+        }
+
+        public static void ValidateCredentials(this UserData userData, string name, string password)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("The name cannot be empty");
+
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("The password cannot be empty");
+
+
+             var isValid = userData.ValidateUser(name, password);
+             if (!isValid) { throw new Exception("Validation failed."); }
+        }
+
+        public static User GetUserExtension(this UserData userData, string name, string password)
+        {
+            return userData.GetUser(name, password);
+        }
     }
 }
--- PS_45_Aleksandar/WelcomeExtended/Program.cs
+++ PS_45_Aleksandar/WelcomeExtended/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Welcome.Model;
+using Welcome.Others;
+using WelcomeExtended.Data;
+using WelcomeExtended.Others;
+
+try
+{
+    UserData userData = new UserData();
+
+    userData.AddUser(new User("Alex Alexiev", "password123", UserRolesEnum.STUDENT));
+    userData.AddUser(new User("Student2", "123", UserRolesEnum.STUDENT));
+    userData.AddUser(new User("Teacher", "1234", UserRolesEnum.PROFESSOR));
+    userData.AddUser(new User("Admin", "12345", UserRolesEnum.ADMIN));
+
+    // Подкана за вход
+    Console.WriteLine("Please enter your name:");
+    string name = Console.ReadLine();
+    Console.WriteLine("Please enter your password:");
+    string password = Console.ReadLine();
+
+    // Валидация на потребителя
+    bool isValid = userData.ValidateUser(name, password);
+    if (isValid)
+    {
+        // Получаване на потребителя
+        User user = userData.GetUser(name, password);
+
+        // Визуализация на потребителските данни
+        if (user != null)
+        {
+            
+            Console.WriteLine(user.ToString()); 
+        }
+        else
+        {
+            Console.WriteLine("Потребителят не е намерен");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Потребителят не е намерен");
+    }
+}
+catch (Exception e)
+{
+    var log = new ActionOnError(Delegates.Log);
+    log(e.Message);
+}
+finally
+{
+    Console.WriteLine("Executed in any case");
+}
+
